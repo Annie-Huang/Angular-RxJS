4505294d422d6e254a36c6c8490054e1caadd3fb
Annie Huang <annie.hn@gmail.com>
2020-01-25 16:44:30 +1100

    Now in product-list.component.ts it can move out from OnInit (I am not 100% sure why it has to be inside ngOnInit() before...

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -11,38 +11,47 @@ import { catchError } from 'rxjs/operators';
   styleUrls: ['./product-list.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent {
   pageTitle = 'Product List';
   errorMessage = '';
   categories;
 
   // products: Product[] = [];
   // sub: Subscription;
-  products$: Observable<Product[]>;
 
+  // Now I look at the code, I don't 100% understand why this.products$ = this.productService.getProducts() have to be inside ngOnIit()?
+  // products$: Observable<Product[]>;
+  products$ = this.productService.products$
+    .pipe(
+      catchError(err => {
+        this.errorMessage = err;
+        // return of([]); // The same as below
+        return EMPTY;
+      })
+    );
 
   constructor(private productService: ProductService) { }
 
-  ngOnInit(): void {
-    // this.sub = this.productService.getProducts()
-    //   .subscribe(
-    //     products => this.products = products,
-    //     error => this.errorMessage = error
-    //   );
-
-    // Do it this way so we can keep the async pipe in the product-list.component.html
-    // 1. So we don't need to subscribe (ngOnInit) and unsubscribe (onDestory)
-    // 2. Also improve change detection (can switch changeDetection to 'OnPush'
-    // Module04 Going Reactive - Note26 - Change Detection Strategies and why anyc pipe on observable can improve change detection cycle.jpg
-    this.products$ = this.productService.getProducts()
-      .pipe(
-        catchError(err => {
-          this.errorMessage = err;
-          // return of([]); // The same as below
-          return EMPTY;
-        })
-      );
-  }
+  // ngOnInit(): void {
+  //   // this.sub = this.productService.getProducts()
+  //   //   .subscribe(
+  //   //     products => this.products = products,
+  //   //     error => this.errorMessage = error
+  //   //   );
+  //
+  //   // Do it this way so we can keep the async pipe in the product-list.component.html
+  //   // 1. So we don't need to subscribe (ngOnInit) and unsubscribe (onDestory)
+  //   // 2. Also improve change detection (can switch changeDetection to 'OnPush'
+  //   // Module04 Going Reactive - Note26 - Change Detection Strategies and why anyc pipe on observable can improve change detection cycle.jpg
+  //   this.products$ = this.productService.getProducts()
+  //     .pipe(
+  //       catchError(err => {
+  //         this.errorMessage = err;
+  //         // return of([]); // The same as below
+  //         return EMPTY;
+  //       })
+  //     );
+  // }
 
   // ngOnDestroy(): void {
   //   this.sub.unsubscribe();
