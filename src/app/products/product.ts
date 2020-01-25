3d75f2c882d8d6853d5042815670ee449c4d1536
Annie Huang <annie.hn@gmail.com>
2020-01-25 21:49:03 +1100

    Add category into Product interface. Add logic to assign productCategories$ in product-category.service.ts

--- src/app/products/product.ts
+++ src/app/products/product.ts
@@ -1,4 +1,17 @@
 /* Defines the product entity */
+// export interface Product {
+//   id: number;
+//   productName: string;
+//   productCode?: string;
+//   description?: string;
+//   price?: number;
+//   categoryId?: number;
+//   quantityInStock?: number;
+//   searchKey?: string[];
+//   supplierIds?: number[];
+// }
+
+// category will not be available until we mapped it.
 export interface Product {
   id: number;
   productName: string;
@@ -6,6 +19,7 @@ export interface Product {
   description?: string;
   price?: number;
   categoryId?: number;
+  category?: string;
   quantityInStock?: number;
   searchKey?: string[];
   supplierIds?: number[];
