b8b4bc50b54f61168e022f35a66df2fa8ea1c728
Annie Huang <annie.hn@gmail.com>
2020-01-26 08:20:28 +1100

    create a productSimpleFilter$ in product-list.component.ts and filter products in product-list.component.html

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -4,7 +4,7 @@ import { EMPTY, Observable } from 'rxjs';
 
 import { Product } from './product';
 import { ProductService } from './product.service';
-import { catchError } from 'rxjs/operators';
+import {catchError, map} from 'rxjs/operators';
 
 @Component({
   templateUrl: './product-list.component.html',
@@ -15,6 +15,7 @@ export class ProductListComponent {
   pageTitle = 'Product List';
   errorMessage = '';
   categories;
+  selectedCategoryId = 1;
 
   // products: Product[] = [];
   // sub: Subscription;
@@ -30,6 +31,17 @@ export class ProductListComponent {
         return EMPTY; // Observable that immediately completes.
       })
     );
+
+  productSimpleFilter$ = this.productService.productWithCategory$
+    .pipe(
+      map(products =>
+        products.filter(product =>
+          this.selectedCategoryId ? product.categoryId === this.selectedCategoryId : true // show all if there is no selectedCategoryId
+        )
+      )
+    );
+
+
 
   constructor(private productService: ProductService) { }
 
